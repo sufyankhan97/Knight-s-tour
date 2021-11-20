c1bdc456d862d4804a413606f1cb9551fecbf5ab
Bharat Ramadas <bramadas@wisc.edu>
2021-11-20 11:31:58 -0600

    Update cmd_proc

--- cmd_proc.sv
+++ cmd_proc.sv
@@ -2,31 +2,31 @@ module cmd_proc(clk,rst_n,cmd,cmd_rdy,clr_cmd_rdy,send_resp,strt_cal,
                 cal_done,heading,heading_rdy,lftIR,cntrIR,rghtIR,error,
 				frwrd,moving,tour_go,fanfare_go);
 				
-	//sufyan
-	parameter FAST_SIM = 1;		// speeds up incrementing of frwrd register for faster simulation
+	parameter FAST_SIM = 1;				// speeds up incrementing of frwrd register for faster simulation
 				
 	input clk,rst_n;					// 50MHz clock and asynch active low reset
 	input [15:0] cmd;					// command from BLE
-	input cmd_rdy;					// command ready
+	input cmd_rdy;						// command ready
 	output logic clr_cmd_rdy;			// mark command as consumed
-	output logic send_resp;			// command finished, send_response via UART_wrapper/BT
-	output logic strt_cal;			// initiate calibration of gyro
-	input cal_done;					// calibration of gyro done
+	output logic send_resp;				// command finished, send_response via UART_wrapper/BT
+	output logic strt_cal;				// initiate calibration of gyro
+	input cal_done;						// calibration of gyro done
 	input signed [11:0] heading;		// heading from gyro
-	input heading_rdy;				// pulses high 1 clk for valid heading reading
+	input heading_rdy;					// pulses high 1 clk for valid heading reading
 	input lftIR;						// nudge error +
 	input cntrIR;						// center IR reading (have I passed a line)
 	input rghtIR;						// nudge error -
-	output reg signed [11:0] error;	// error to PID (heading - desired_heading)
-	output reg [9:0] frwrd;			// forward speed register
+	output reg signed [11:0] error;		// error to PID (heading - desired_heading)
+	output reg [9:0] frwrd;				// forward speed register
 	output logic moving;				// asserted when moving (allows yaw integration)
 	output logic tour_go;				// pulse to initiate TourCmd block
 	output logic fanfare_go;			// kick off the "Charge!" fanfare on piezo
 
-	reg move_cmd;
+	logic move_cmd;
+	logic move_cmd_ff;
 	wire move_done;
-	reg [2:0]op_ff_move_counter;
-	reg [2:0]op_ff_move_count;
+	reg [3:0] move_counter;
+	reg [3:0] move_count_cmd;
 	wire posedge_cntrIR;
 	reg posedge_cntrIR_temp;
 	
@@ -34,7 +34,7 @@ module cmd_proc(clk,rst_n,cmd,cmd_rdy,clr_cmd_rdy,send_resp,strt_cal,
 	logic signed [9:0]frwrd_inc;		// magnitude of how fast the bot should move
 	logic frwrd_zero, max_spd;			// needed to limit bounds of frwrd
 
-	logic en, clr, inc_frwrd, dec_frwrd;
+	logic en, clr_frwd, inc_frwrd, dec_frwrd;
 	// cmd_proc variables
 	logic [3:0] opcode;
 
@@ -42,77 +42,19 @@ module cmd_proc(clk,rst_n,cmd,cmd_rdy,clr_cmd_rdy,send_resp,strt_cal,
 	typedef enum logic [2:0]{IDLE, CAL, STR_MOVE, MOVE, STP_MOVE, TOUR} state_t;
 	state_t state, nxt_state;
 	
-	logic move_cmd;
 
+	logic tour_go_comb;	// tour_go in state machine. need to flop to prevent glitch
+	
 	//////////////////////////////////////////////////////////////////
 	//////////////////////// data path ///////////////////////////////
 	//////////////////////////////////////////////////////////////////
 	
-	///////////////posedge detector////////////////
-	always @(posedge clk)								//rising edge detector
-	posedge_cntrIR_temp <= cntrIR;
-
-
-	assign posedge_cntrIR = cntrIR & ~posedge_cntrIR_temp;
-	///////////////////////////////////////////////
-
-	///////////counter/////////////////////////////\
-	reg op_ff_move_cmd;
-
-	always @(posedge clk)
-	op_ff_move_cmd = move_cmd;				//flop move_cmd since it is a fsm output and can glitch
-
-	always @(posedge clk, cntrIR)
-	begin
-	if(op_ff_move_cmd)
-		op_ff_move_counter <= 1'b0;			
-	else if(posedge_cntrIR)
-		op_ff_move_counter <= op_ff_move_counter + 1'b1;
-	end
-	////////////////////////////////////////////
-
-	///////////flop for cmd////////////////////
-	always @(posedge clk)
-	begin
-	if(op_ff_move_cmd)
-	op_ff_move_count <= cmd[2:0] << 1 ;			//shift by one place since we cross center line 2 times for every square
-	end
-	//////////////////////////////////////////
-
-	assign move_done = (op_ff_move_count === op_ff_move_counter);	
-
-
-
-	///////////////////////////////////////////////////////PID//////////////////////////////////////////////////////
-
-	/////////////desired heading/////////////////////////////////////
-	wire [11:0]adjusted_cmd;
-	reg [11:0]op_ff_desired_heading;
-
-	adjusted_cmd = (|cmd[11:4] == 0) ? 12'h0	:	{cmd[11:4] , 4'hF};
-
-	always @(posedge clk)
-	if(move_cmd)
-	op_ff_desired_heading <= adjusted_cmd;
-	////////////////////////////////////////////////////////////////
-
-	/////////////////////leftIR and rghtIR ///////////////////////
-	wire [11:0]err_nudge; 
-
-	err_nudge	=	(leftIR)	?	12'h05F	:	(rghtIR)	?	12'hFA1	:	12'h0;
-
-	///////////////////final summation////////////////////////////
-	wire [11:0]error;
-
-	error = heading - desired_heading	+ err_nudge;
-	
 	////////////////// frwrd reg implementation //////////////////
-
 	// set magnitude of increment based on FAST_SIM
 	generate if (FAST_SIM)
 		assign frwrd_inc = inc_frwrd ? 10'h020 : 10'h3C0;	// +ve when inc_frwrd is set, -ve otherwise
 	else
-		assign frwrd_inc = inc_frwrd ? 10'h004 : 10'h3F8;
+		assign frwrd_inc = inc_frwrd ? 10'h004 : 10'h3F8;	
 	endgenerate	
 	  
 	// assign frwrd_inc = FAST_SIM ? 8'h20 : 8'h04;
@@ -122,19 +64,73 @@ module cmd_proc(clk,rst_n,cmd,cmd_rdy,clr_cmd_rdy,send_resp,strt_cal,
 	always_ff @(posedge clk, negedge rst_n)
 		if (!rst_n)
 			frwrd <= 10'h000;				// reset reg
-		else if (clr)
+		else if (clr_frwd)
 			frwrd <= 10'h000;				// counter reset
 		else if (en)
 			frwrd <= frwrd + frwrd_inc;		// increment counter
 
 	// logic for enable 
-	assign frwrd_zero = !(|frwrd);
-	assign max_spd = &frwrd[9:8];
+	assign frwrd_zero = !(|frwrd);			// dec_frwrd used to set enable when frwd is not 0
+	assign max_spd = &frwrd[9:8]; 	
 
 	assign en = heading_rdy ? (frwrd_zero && inc_frwrd) || (max_spd && dec_frwrd) : 1'b0;
+	
+	////////////////////posedge detector for IR//////////////////////
+	always @(posedge clk)		
+		posedge_cntrIR_temp <= cntrIR;
+
+	assign posedge_cntrIR = cntrIR & ~posedge_cntrIR_temp;	// 1 if rising edge for cntrIR
+
+	////////////////////counter for sqaures/////////////////////////////
+	always @(posedge clk)
+		move_cmd_ff = move_cmd;				//flop move_cmd since it is a fsm output and can glitch
+
+	always @(posedge clk) begin
+		if(move_cmd_ff)
+			move_counter <= 1'b0;			
+		else if(posedge_cntrIR)
+			move_counter <= move_counter + 1'b1;	// increment counter at posedge of IR
+	end
+
+	//flop for cmd
+	always @(posedge clk) 	begin
+		if(move_cmd_ff)
+			move_count_cmd <= {cmd[2:0], 1'b0} ;	//multiply by 2 since we cross center line 2 times for every square
+	end
+	//////////////////////////////////////////
+
+	assign move_done = (move_counter == move_count_cmd);	
+
+	/////////////////////////////////////PID Interface/////////////////////////////////////////
+
+	//desired heading
+	reg [11:0] desired_heading;
+
+	always @(posedge clk)
+		if(move_cmd_ff)
+			desired_heading <= !(|cmd[11:4]) ? 12'h0	:	{cmd[11:4] , 4'hF};	// set heading from cmd 
+
+	//leftIR and rghtI
+	logic [11:0]err_nudge; 		// nudge factor to align bot when it hits the end rails
+
+	generate if (FAST_SIM)
+		assign err_nudge =	lftIR ? 12'h1FF : rghtIR ?	12'hE00 : 12'h000;
+	else 
+		assign err_nudge =	lftIR ? 12'h05F : rghtIR ?	12'hFA1	: 12'h000;
+	endgenerate
+	
+	//final summation
+	assign error = heading - desired_heading + err_nudge;
 
 	//// command processing ////
-	assign opcode = cmd[15:12];
+	assign opcode = cmd[15:12];		// determines what the bot is supposed to do
+	
+	// setting fanfare
+	reg fanfare_set;
+	always @(posedge clk, posedge cmd_rdy) 	begin	// flip flop needed to set fanfare as cmd gets cleared 
+		if(cmd_rdy)
+			fanfare_set <= opcode==4'h3 ? 1'b1 : 1'b0 ;	//set fanfare based on command
+	end
 	
 	/////////////////////////////////////////////////////////////////////
 	//////////////////////// state machine //////////////////////////////
@@ -146,18 +142,20 @@ module cmd_proc(clk,rst_n,cmd,cmd_rdy,clr_cmd_rdy,send_resp,strt_cal,
 			state <= IDLE;
 		else
 			state <= nxt_state;
-				
 
 	// state transitions
 	always_comb begin
 		strt_cal = 0;			// start calibration of gyro signal
 		clr_cmd_rdy = 0;		// clear cmd rdy signal
-		send_resp = 0;
-		move_cmd = 0;
-		inc_frwrd = 0;
-		dec_frwrd = 0;
-		
+		send_resp = 0;			// send response to UART. Not needed for TOUR
+		move_cmd = 0;			// signal to start bot orienting itself. Reset square count, and desired_heading
+		inc_frwrd = 0;			// begins ramping up speed of bot to max speed
+		dec_frwrd = 0;			// begins decelerating bot.
+		clr_frwd = 0;			// sets bot speed to 0
+		tour_go_comb = 0;		// sets tour_go
+		fanfare_go = 0;			// sets fanfare_go
 		nxt_state = state;
+		
 		case (state)
 		IDLE:	begin 
 				if (cmd_rdy)
@@ -172,35 +170,48 @@ module cmd_proc(clk,rst_n,cmd,cmd_rdy,clr_cmd_rdy,send_resp,strt_cal,
 					nxt_state = TOUR;
 				end
 				
-		CAL:	begin 
-				strt_cal = 1'b1;
-				if (cal_done)	begin
+		CAL:	begin 					
+				strt_cal = 1'b1;		// begin calibration
+				if (cal_done)	begin	// after cal is over, got back to IDLE
 					send_resp = 1'b1;
-					nxt_state = IDLE;
+					nxt_state = IDLE;	
+					move_cmd = 1'b1;	// bot begins orienting itself
+					end
 				end
 		
-		STR_MOVE:	begin	
-					move_cmd = 1'b1;
-					if ((error < 12'h030) && (error > 12'FD0))
-					nxt_state = MOVE;	end
+		STR_MOVE:	if ((error < 12'h030) && (error > 12'hFD0))	begin 	// make sure heading is more or less correct
+						nxt_state = MOVE;								// start moving forward
+						clr_frwd = 1'b1;	end							// set initial bot speed to 0
 					
 		MOVE:	begin
-				move_cmd = 1'b1;
 				inc_frwrd = 1'b1;
-				if (move_done)
-					nxt_state = STP_MOVE;
+				if (move_done) begin			
+					nxt_state = STP_MOVE;			// begin bot deceleration after squares are covered
+					fanfare_go = fanfare_set; end	// set fanfare_go based on type of cmd after move_done
 				end
 				
 		STP_MOVE: 	begin
-					move_cmd = 1'b1;
 					dec_frwrd = 1'b1;
-					if (!(|frwrd)) begin
-						send_resp = 1'b1;
+					if (!(|frwrd)) begin	// check if bot is stationary
+						send_resp = 1'b1;	// send response
 						nxt_state = IDLE;	end
 					end
+		
+		TOUR:	begin 						// commands received from TourCmd
+				tour_go_comb = 1'b1;
+				nxt_state = IDLE; 
+				end
+		
+		default: 	nxt_state = IDLE;
 		endcase
 	end
 	
-
+	// tour_go flop to prevent glitch
+	always_ff @(posedge clk, negedge rst_n)
+		if (!rst_n)
+			tour_go <= 1'b0;
+		else 
+			tour_go <= tour_go_comb;
+			
 endmodule
   
