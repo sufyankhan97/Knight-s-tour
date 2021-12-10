b49c0e5678ec25047078084bc075976039f5d41a
Bharat Ramadas <bramadas@wisc.edu>
2021-12-10 14:04:40 -0600

    Update KnightsTour

--- ProjectFiles/KnightsTour.sv
+++ ProjectFiles/KnightsTour.sv
@@ -21,7 +21,7 @@ module KnightsTour(
   //////////////////////
   wire rst_n;							// global synchronized reset
   wire strt_cal;						// initiate gyro heading calibration
-  wire cal_done;						// done with gyro heading calibration
+  logic cal_done;						// done with gyro heading calibration
   wire signed [10:0] lft_spd, rght_spd;	// signed motor controls
   wire signed [11:0] error;
   wire signed [11:0] heading;
@@ -36,9 +36,9 @@ module KnightsTour(
   wire [15:0] cmd;						// multiplexed cmd from TourCmd
   wire [15:0] cmd_UART;					// command from UART/Bluetooth
   wire clr_cmd_rdy;
-  wire tour_go;
+  logic tour_go;
   wire fanfare_go;
-  wire start_tour;						// done from TourLogic
+  logic start_tour;						// done from TourLogic
   wire [4:0] mv_indx;					// "address" of tour move
   wire [7:0] move;						// 1-hot encoded Knight move
   wire [7:0] resp;						// either 0xA5 (done), or 0x5A (in progress)
